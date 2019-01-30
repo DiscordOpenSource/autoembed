8080ea366ca68389b32b28fc2df3ec8d2a9df8d1
CollierPlays <collier.bell20@gmail.com>
2019-01-30 14:25:21 -0600

    Update index.js
    Put the commit on the wrong line of code. also removed console message logging (useless)

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const config = require('config.json');  // Imports the Config
 client.on("message", message => {
   // Waits for a message
   if (message.author.id === client.user.id) return; // Prevents the bot from spamming the chat.
-  console.log(`${message.author} | ${message.content}`); // Creates a pretty embed.
-  let embed = new Discord.RichEmbed()
+  
+  let embed = new Discord.RichEmbed() // Creates a pretty embed.
     .setTitle(message.id)
     .setDescription(message.content)
     .setThumbnail(message.author.avatarURL)
